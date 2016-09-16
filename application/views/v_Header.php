950b3d975b971b4dd65bfebbfb799393e55c850b
Gab2015 <024gab@gmail.com>
2016-09-16 15:59:49 -0600

    menu responsivo v1,16/09/2016

--- application/views/v_Header.php
+++ application/views/v_Header.php
@@ -5,7 +5,8 @@
                     <h1 id="Head">SISTEMAS C&C</h1>
                </div>
                <div class="col-lg-1 col-sx-12">
-                    <a class="btn btn-primary Cerrar" href="c_Menu/Logout">Cerrar</a>
+                    <a class="btn btn-primary Cerrar" href="c_Menu/Logout">
+                     Cerrar <span class="glyphicon glyphicon-user"></span></a>
                </div>
           </nav>    
      </div>     
--- js/v_Menu.js
+++ js/v_Menu.js
@@ -5,12 +5,16 @@ marginLeft:'-160px'});
 $(".glyphicon").css({
 marginLeft:'150px'});
 $(".MenuPrincipal").css({
-height: '20px'});
+height:'20px'});
+$("h1#HeadMenu").css({
+marginLeft:'-160px'});
 });
 $(".navbar-nav li.desplazar a").dblclick(function(event) {
 $("nav.sidebar").css({
 marginLeft:'0px'});
 $(".glyphicon").css({
 marginLeft:'0px'});
+$("h1#HeadMenu").css({
+marginLeft:'0px'});
 });
-}); 
+});
