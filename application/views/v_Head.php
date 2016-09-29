62ee7dc4a2c06efd66903d5990f3ab46c3241e3b
Gab2015 <024gab@gmail.com>
2016-09-29 09:31:58 -0600

    se cambio el nombre de sistemascyc.css ha v_Menu.css, se agrego este nombre ha v_Head.php

--- application/views/v_Head.php
+++ application/views/v_Head.php
@@ -6,7 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link href="<?php echo base_url("css/css/bootstrap.css");?>" rel="stylesheet" media="all">
 	<link href="<?php echo base_url("css/css/bootstrap-submenu.css");?>" rel="stylesheet" media="all">
-	<link href="<?php echo base_url("css/css/sistemascyc.css")?>" rel="stylesheet" media="all">
+	<link href="<?php echo base_url("css/css/v_Menu.css")?>" rel="stylesheet" media="all">
 	<link rel="icon" type="image/jpg" href="<?php echo base_url("imagenes/favicon.jpg");?>">
 	<script src="<?php echo base_url("js/jquery.js");?>"></script>
 	<script src="<?php echo base_url("css/js/bootstrap.js");?>"></script>
